1a69fbd27d79e426331f2652032395ac78651411
Vanina Quezada <vaninaquezada@gmail.com>
2022-01-05 09:13:17 -0300

    creacion clase base

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,44 @@
 package base;
 
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class BaseTest {
+        private WebDriver driver;
+
+        public void setUp() throws InterruptedException {
+            System.setProperty("webdriver.chrome.driver","resosurces/chromedriver.exe");
+
+            ChromeOptions options = new ChromeOptions();
+       //   options.addArguments("--headless");
+            options.addArguments("--incognito");
+            driver = new ChromeDriver(options);
+            driver.get("https://www.google.com/");
+
+
+            Thread.sleep(30000);
+
+
+        }
+
+        @Test
+        public void Open() throws InterruptedException{
+
+            setUp();
+
+
+
+
+            close();
+        }
+
+        public void close(){
+            driver.quit();
+
+        }
 }
+
+
